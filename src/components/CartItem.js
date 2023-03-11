915ce0c6b6fb3a4600ec17da21be91b95e7e76a4
g1348032 <g1348032@icloud.com>
2023-03-11 18:36:58 +0900

    img commit

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -6,7 +6,7 @@ const CartItem = ({ id, img, title, price, amount }) => {
   const dispatch = useDispatch();
   return (
     <article className="cart-item">
-      <img src="../img/flowerBuy1.png" alt={title} />
+      <img src={img} alt={title} />
       <div>
         <h4>{title}</h4>
         <h4 className="item-price">{price}円</h4>
